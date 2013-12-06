587d6078ea611e6a79d5d0a27b9edf30c270c882
Dave Cramer <davecramer@gmail.com>
2013-12-06 14:57:40 -0500

    change create to update

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -14,7 +14,7 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create" // one of 'create', 'create-drop', 'update', 'validate', ''
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
             url = "jdbc:postgresql://localhost:5432/manager"
         }
     }
